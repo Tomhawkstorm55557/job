4f43cdf4aec66a630514fa3cd339d6d408ea1438
huyihi <vinayaksharma20715@gmail.com>
2024-09-05 00:27:07 +0530

    Initial commit of MERN-Stack-Job-Seeking-Web-Application

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,7 @@ import fileUpload from "express-fileupload";
 const app = express();
 config({ path: "./config/config.env" });
 
+
 app.use(
   cors({
     origin: [process.env.FRONTEND_URL],
--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,16 +1,22 @@
 export const sendToken = (user, statusCode, res, message) => {
   const token = user.getJWTToken();
+
+  // Ensure COOKIE_EXPIRE is a number
+  const cookieExpireDays =  5; // Default to 5 days if not set
+
+  // Set options for the cookie
   const options = {
-    expires: new Date(
-      Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true, // Set httpOnly to true
+    expires: new Date(Date.now() + cookieExpireDays * 24 * 60 * 60 * 1000),
+    httpOnly: true, // Ensure cookie is only accessible via web server
   };
 
-  res.status(statusCode).cookie("token", token, options).json({
-    success: true,
-    user,
-    message,
-    token,
-  });
+  // Send response with the cookie and token
+  res.status(statusCode)
+    .cookie("token", token, options)
+    .json({
+      success: true,
+      user,
+      message,
+      token,
+    });
 };
